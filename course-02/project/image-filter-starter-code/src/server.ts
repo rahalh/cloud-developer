843d411e805bdf3a3aed8d918ee3980c369abf72
rahalh <hamzarahal031@gmail.com>
2022-08-19 11:26:16 +0100

    adds the filteredimage endpoint

--- course-02/project/image-filter-starter-code/src/server.ts
+++ course-02/project/image-filter-starter-code/src/server.ts
@@ -13,11 +13,9 @@ import {filterImageFromURL, deleteLocalFiles} from './util/util';
   // Use the body parser middleware for post requests
   app.use(bodyParser.json());
 
-  // @TODO1 IMPLEMENT A RESTFUL ENDPOINT
   // GET /filteredimage?image_url={{URL}}
   // endpoint to filter an image from a public url.
   // IT SHOULD
-  //    1
   //    1. validate the image_url query
   //    2. call filterImageFromURL(image_url) to filter the image
   //    3. send the resulting file in the response
@@ -26,10 +24,26 @@ import {filterImageFromURL, deleteLocalFiles} from './util/util';
   //    image_url: URL of a publicly accessible image
   // RETURNS
   //   the filtered image file [!!TIP res.sendFile(filteredpath); might be useful]
+  app.get("/filteredimage", async (req, res) => {
+    const { image_url } = req.query;
+    if (!image_url) {
+      return res.status(400).send('The image_url query parameter is missing');
+    }
 
-  /**************************************************************************** */
+    try {
+      const img_path = await filterImageFromURL(image_url);
+      return res.sendFile(img_path, (err) => {
+        if (err) {
+          res.status(500).send('An unexpected error occurred');
+        }
+        deleteLocalFiles([img_path]);
+      })
+    }
+    catch (err) {
+      return res.status(400).send('The provided image_url is invalid');
+    }
+  });
 
-  //! END @TODO1
   
   // Root Endpoint
   // Displays a simple message to the user
@@ -40,7 +54,7 @@ import {filterImageFromURL, deleteLocalFiles} from './util/util';
 
   // Start the Server
   app.listen( port, () => {
-      console.log( `server running http://localhost:${ port }` );
-      console.log( `press CTRL+C to stop server` );
+    console.log( `server running http://localhost:${ port }` );
+    console.log( `press CTRL+C to stop server` );
   } );
 })();
